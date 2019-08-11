9b32dba5ab0658e0e120e0d974937b19601e201c
cjmash <jamescollins.jcparty@gmail.com>
2019-08-11 12:39:49 +0300

    add procfile

--- index.js
+++ index.js
@@ -7,15 +7,13 @@ const app = express();
 app.use(bodyParser.json());
 locationRoutes(app);
   // dbUrl = `mongodb://${process.env.TEST_USER}:${process.env.TEST_PASSWORD}${process.env.TEST_HOST}/${process.env.TEST_NAME}`;
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development || production') {
     let dbUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}${process.env.DB_HOST}/${process.env.DB_NAME}`;
 
 
     mongoose.connect(dbUrl,{ 
     useNewUrlParser: true }).then(() => console.log('connected')).catch((err) => console.log(err));
     
-app.listen((process.env.PORT  || 3030), () => {
-  console.log('server is up');
-});
+app.listen(process.env.PORT  || 3030);
 }
 export default app;
